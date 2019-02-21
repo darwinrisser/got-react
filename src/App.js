c9b7b139d9a828c37a2fcc337d5316c331e1302e
Darwin Risser <riser.darwin@gmail.com>
2019-02-21 07:25:10 -0600

    first changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// const questions = {
+//   question1: {
+//     text: "If you were the King/Queen of Westeros, who would you choose as your Hand? A. Tyrion Lannister  B. Davos Seaworth C. Tywin Lannister D. Ned Stark ",
+//     choices: [
+//       {
+//         name: "Tyrion Lannister",
+//         img: "https://pbs.twimg.com/profile_images/668279339838935040/8sUE9d4C_400x400.jpg",
+//         id: 1
+//       }
+
 class App extends Component {
   state = {
     data: null
   }
 
-  componentDidMount() {
-    fetch('http://172.16.0.162:3001/characters', {mode: 'no-cors'})
-      .then(response => response.json())
-      .then(data => this.setState({ data }));
-  }
-
   render() {
     return (
       <div className="App">
-      Hi
-        {this.state.data}
+         {this.props.questions.question1.text}
+         <div>
+           {this.props.questions.question1.choices[0].name}
+           <br></br>
+           <img src={this.props.question.question1.choices[0].img} alt="Tyrion"/>    
+         </div>
+         <div>
+          
+           {this.props.questions.question1.choices[1].name}
+           <br></br>
+           <img src={this.props.question.question1.choices[1].img} alt="Tyrion"/>    
+         </div>
+         <div>
+          
+           {this.props.questions.question1.choices[2].name}
+           <br></br>
+           <img src={this.props.question.question1.choices[2].img} alt="Tyrion"/>    
+        </div>
+        <div>
+          
+          {this.props.questions.question1.choices[3].name}
+          <br></br>
+          <img src={this.props.question.question1.choices[3].img} alt="Tyrion"/>    
+         </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,35 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const questions = {
+  question1: {
+    text: "If you were the King/Queen of Westeros, who would you choose as your Hand?",
+    choices: [
+      {
+        name: "Tyrion Lannister",
+        img: "https://pbs.twimg.com/profile_images/668279339838935040/8sUE9d4C_400x400.jpg",
+        id: 1
+      },
+      {
+        name: "Davos Seaworth",
+        img: "https://pbs.twimg.com/profile_images/668279339838935040/8sUE9d4C_400x400.jpg",
+        id: 2
+      },
+      {
+        name: "Tywin Lannister",
+        img: "https://pbs.twimg.com/profile_images/668279339838935040/8sUE9d4C_400x400.jpg",
+        id: 3
+      },
+      {
+        name: "Ned Stark",
+        img: "https://pbs.twimg.com/profile_images/668279339838935040/8sUE9d4C_400x400.jpg",
+        id: 4
+      }
+    ]
+  }
+}
+
+ReactDOM.render(<App questions={questions}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
