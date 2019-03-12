7b87af62bb583b7a01ad36491e1832096fec3abb
Darwin Risser <riser.darwin@gmail.com>
2019-03-11 21:56:49 -0500

    Updates

---
+++ precache-manifest.00a7264173e5a754ead7a8e1c1d381e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e623d61ff9bb47e4146b",
+    "url": "/gh-pages/got-react/static/js/runtime~main.e623d61f.js"
+  },
+  {
+    "revision": "a538f6603821ea403259",
+    "url": "/gh-pages/got-react/static/js/main.a538f660.chunk.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/gh-pages/got-react/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "a538f6603821ea403259",
+    "url": "/gh-pages/got-react/static/css/main.c4b6e597.chunk.css"
+  },
+  {
+    "revision": "712be8fb2f6e86b7b7c5dd9bd9c483d2",
+    "url": "/gh-pages/got-react/index.html"
+  }
+];
